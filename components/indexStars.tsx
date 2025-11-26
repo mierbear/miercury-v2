9debe8b317737a0105ba80c5b931ce261c77f648
mierbear <175657293+mierbear@users.noreply.github.com>
2025-11-26 07:12:27 -0400

    animate title with splittext

--- components/indexStars.tsx
+++ components/indexStars.tsx
@@ -7,12 +7,12 @@ const Stars = () => {
 
   useEffect(() => {
     if (stars.current) {
-      gsap.to(stars.current, {rotation:360, duration: 180, repeat: -1, ease: "linear", transformOrigin: "center center"});
+      gsap.to(stars.current, {rotation:360, duration: 360, repeat: -1, ease: "linear", transformOrigin: "center center"});
     }
   }, []);
 
   return (
-    <img src={"/images/stars.png"} ref={stars} className="stars fixed origin-center scale-200 -z-50]" />
+    <img src={"/images/stars.png"} ref={stars} className="stars fixed origin-center scale-180 -z-50]" />
   )
 }
 
---
+++ components/indexTitle.tsx
@@ -0,0 +1,49 @@
+"use client";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { Boldonse } from "next/font/google"
+import { useEffect, useRef } from "react";
+
+const boldonse = Boldonse({
+  weight: "400",
+  subsets: ["latin"],
+})
+
+const Title = () => {
+  const titleRef = useRef<HTMLHeadingElement | null>(null);
+
+  useEffect(() => {
+    if (!titleRef.current) return;
+
+    gsap.set(titleRef.current, { autoAlpha: 1 });
+
+    const split = new SplitText(titleRef.current, { type: "chars" });
+
+    const tl = gsap.timeline();
+
+    tl.from(split.chars, {
+      duration: 0.8,
+      opacity: 0,
+      yPercent: 120,
+      stagger: 0.05,
+      ease: "power4.out",
+    })
+
+    return () => {
+      tl.kill();
+      split.revert();
+    };
+  }, []);
+
+  return (
+    <h1
+      ref={titleRef}
+      className={`${boldonse.className} header text-amber-200 text-9xl z-10`}
+      style={{ visibility: "hidden" }}
+    >
+      MIERCURY
+    </h1>
+  );
+};
+
+export default Title;
