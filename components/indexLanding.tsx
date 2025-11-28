64045cf4d0195f26aab173803ded8c65724a793d
mierbear <175657293+mierbear@users.noreply.github.com>
2025-11-28 16:36:44 -0400

    implement open/close button functionality

--- components/indexLanding.tsx
+++ components/indexLanding.tsx
@@ -9,23 +9,79 @@ const boldonse = Boldonse({
   subsets: ["latin"],
 })
 
-const landing = () => {
+const Landing = () => {
+  const buttonRef = useRef<HTMLDivElement | null>(null);
+  const landingRef = useRef<HTMLDivElement | null>(null);
 
+  const openTl = gsap.timeline();
+  const closeTl = gsap.timeline();
+
+  const isOpen = useRef(true);
+
+  const toggleLanding = () => {
+    if (!landingRef.current) return;
+
+    const height = landingRef.current.offsetHeight;
+    const width = landingRef.current.offsetWidth;
+
+    if (isOpen.current) {
+      closeTl.to(landingRef.current, {
+        y: -height,
+        duration: .7,
+        ease: "power1.inOut",
+      })
+      .to(buttonRef.current, {
+        x: width / 2.2,
+        duration: .8,
+        ease: "power2.out",
+      });
+    } else {
+      openTl.to(buttonRef.current, {
+        x: 0,
+        duration: .4,
+        ease: "power3.out",
+      })
+      .to(landingRef.current, {
+        y: 0,
+        duration: .6,
+        ease: "power2.inOut",
+      });
+    }
+
+    isOpen.current = !isOpen.current;
+  };
 
   return (
-    <div className="fixed min-w-screen min-h-[90vh] grid grid-rows-[5fr_1fr] z-500 bg-white/80  shadow-2xl">
-      <div className="bg-white-400/50 grid grid-cols-4">
-        <div className="flex flex-col justify-center items-center">Characters</div>
-        <div className="flex flex-col justify-center items-center">MTWIM Compendium</div>
-        <div className="flex flex-col justify-center items-center">Pacific Purgatory</div>
-        <div className="flex flex-col justify-center items-center">Games</div>
+    <div
+      ref={landingRef}
+      className="fixed min-w-screen min-h-[90vh] grid grid-rows-[5fr_1fr] z-500 bg-white/80 shadow-2xl"
+    >
+      <div className="grid grid-cols-4">
+        <div className="landing-tile flex justify-center items-center bg-[#838177]">
+          <span>Characters</span>
+        </div>
+        <div className="landing-tile flex justify-center items-center bg-[#8b979b]">
+          <span>MTWIM Compendium</span>
+        </div>
+        <div className="landing-tile flex justify-center items-center bg-[#616d7a]">
+          <span>Pacific Purgatory</span>
+        </div>
+        <div className="landing-tile flex justify-center items-center bg-[#8a8b7d]">
+          <span>Games</span>
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-center bg-gray-600/50">
+
+      <div className="flex flex-col justify-center items-center bg-gray-600/50 relative">
         <h1>no thanks, take me back to the blog!</h1>
 
+        <div
+          ref={buttonRef}
+          onClick={toggleLanding}
+          className="absolute bottom-0 bg-white rounded-full min-w-[2em] min-h-[2em] text-3xl translate-y-[50%] text-center cursor-pointer"
+        />
       </div>
     </div>
   );
 };
 
-export default landing;
+export default Landing;
