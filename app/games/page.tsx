32e9d056ac88a0b645c57101aff324a4b175cecb
mierbear <175657293+mierbear@users.noreply.github.com>
2025-12-10 16:56:00 -0400

    make games list page responsive

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
 import React from "react";
 import GamesList from "@/components/gamesList";
+import { Boldonse } from "next/font/google"
+
+const boldonse = Boldonse({
+  weight: "400",
+  subsets: ["latin"],
+})
 
 export default function Home() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <main className="min-w-screen min-h-screen grid grid-rows-[0.5fr_3fr_0.5fr] text-white z-50">
+    <main className="min-w-screen min-h-screen grid grid-rows-[0.5fr_3fr] xl:grid-rows-[0.5fr_3fr_0.5fr] text-white z-50">
       
-      <div className="flex justify-center items-center z-50">
-        <h1>games</h1>
+      <div className={`${boldonse.className} flex justify-center items-end z-50 text-7xl md:text-8xl xl:text-6xl xl:justify-start xl:pb-0 xl:items-center xl:pl-3 pb-5`}>
+        <h1>GAMES</h1>
       </div>
 
       <GamesList />
 
-      <div className="flex justify-center items-center z-50">
+      <div className="justify-center items-center z-50 xl:visible invisible hidden xl:flex">
         <h1>games</h1>
       </div>
 
--- components/gamesList.tsx
+++ components/gamesList.tsx
@@ -3,6 +3,7 @@
 import NextLink from "next/link";
 import gsap from "gsap";
 import React from "react";
+import { useEffect, useRef, useState } from "react";
 
 const GamesList = () => {
   const fishingRef = React.useRef<HTMLAnchorElement | null>(null);
@@ -10,32 +11,64 @@ const GamesList = () => {
   const moreRef = React.useRef<HTMLDivElement | null>(null);
   const listRef = React.useRef<HTMLDivElement | null>(null);
 
+  const isDesktop = () => window.innerWidth >= 1280;
+
+  const resetInlineStyles = () => {
+    listRef.current!.style.removeProperty("grid-template-columns");
+    listRef.current!.style.removeProperty("grid-template-rows");
+  };
 
   const listReset = () => {
+    if (!isDesktop()) {
+      resetInlineStyles();
+      return;
+    }
+    
     listRef.current!.style.gridTemplateColumns = "1fr 1fr 1fr";
   };
 
   const listFishSel = () => {
+    if (!isDesktop()) return resetInlineStyles();
+    listRef.current!.style.gridTemplateColumns = "0.9fr 1.8fr 0.9fr";
+  };
+  
+  const listMatchSel = () => {
+    if (!isDesktop()) return resetInlineStyles();
     listRef.current!.style.gridTemplateColumns = "1.8fr 1fr 0.8fr";
   };
 
-  const listMatchSel = () => {
-    listRef.current!.style.gridTemplateColumns = "0.9fr 1.8fr 0.9fr";
-  };
-
   const listMoreSel = () => {
+    if (!isDesktop()) return resetInlineStyles();
     listRef.current!.style.gridTemplateColumns = "0.8fr 1fr 1.8fr";
   };
 
-  return (
-    <div ref={listRef} className="grid grid-cols-[1fr_1fr_1fr] bg-[#535961] z-50 list">
+  useEffect(() => {
+    const handleResize = () => {
+      if (isDesktop()) {
+        listRef.current!.style.gridTemplateColumns = "1fr 1fr 1fr";
+        listRef.current!.style.removeProperty("grid-template-rows");
+        console.log(`is desktop :3`)
+      } else {
+        listRef.current!.style.removeProperty("grid-template-columns");
+        listRef.current!.style.removeProperty("grid-template-rows");
+      }
+    };
 
-        <NextLink onMouseEnter={listFishSel} onMouseLeave={listReset} ref={fishingRef} href="/mierfishing/index.html" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center min-w-[80%] min-h-[50vh] bg-[#858d94]">
-          <span>fishing</span>
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div ref={listRef} className="grid grid-cols-[1fr] grid-rows-3 xl:grid-cols-[1fr_1fr_1fr] xl:grid-rows-1 bg-[#535961] z-50 list gameList">
+
+        <NextLink onMouseEnter={listMatchSel} onMouseLeave={listReset} ref={matchRef} href="/match/index.html" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center min-w-[80%] min-h-[50vh] bg-[#858d94]">
+          <span>match</span>
         </NextLink>
 
-        <NextLink onMouseEnter={listMatchSel} onMouseLeave={listReset} ref={matchRef} href="/match/index.html" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center min-w-[80%] min-h-[50vh] bg-[#707c86]">
-          <span>match</span>
+        <NextLink onMouseEnter={listFishSel} onMouseLeave={listReset} ref={fishingRef} href="/mierfishing/index.html" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center min-w-[80%] min-h-[50vh] bg-[#707c86]">
+          <span>fishing</span>
         </NextLink>
 
         <div onMouseEnter={listMoreSel} onMouseLeave={listReset} ref={moreRef} className="flex justify-center items-center min-w-[80%] min-h-[50vh] bg-[#858d94]">
