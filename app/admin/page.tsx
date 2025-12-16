0fa09a7351026d8af98b9abc3edd208242479dcd
mierbear <175657293+mierbear@users.noreply.github.com>
2025-12-16 04:05:17 -0400

    implement supabase login

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-
+import supabase from "@/lib/supabaseClient";
 
 export default function page() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -8,40 +8,71 @@ export default function page() {
   const [password, setPassword] = useState("");
   const loginRef = useRef<HTMLFormElement | null>(null);
 
+  const fetchSession = async () => {
+    const currentSession = await supabase.auth.getSession();
+    if (currentSession.data.session) {
+      setIsLoggedIn(true);
+    }
+  }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  useEffect(() => {
+    fetchSession();
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsLoggedIn(true);
+
+    if (!isLoggedIn) {
+      const {error} = await supabase.auth.signInWithPassword({email, password})
+      if (error) {
+        console.error("login failed: ", error.message);
+        return;
+      }
+      setIsLoggedIn(true);
+    }
+
   };
 
   return (
     <div className="min-w-screen min-h-screen justify-center align-center items-center flex flex-col text-white">
 
+    {
+    !isLoggedIn ?(
       <form 
         ref={loginRef}
-        className="absolute min-h-[10vh] min-w-[20vh] bg-[#535961]/20 flex flex-col justify-center items-center rounded-l-md gap-2 p-3.5 nonsel"
+        className="absolute min-h-[10vh] min-w-[20vh] bg-[#535961]/20 flex flex-col justify-center items-center gap-2 p-3.5 nonsel"
         onSubmit={handleSubmit}
       >
-        {
-        !isLoggedIn ?(
         <div className="flex flex-col gap-0.5">
-          <input className="bg-[#101113]/90 p-2 rounded-md" type="text" placeholder="email" />
-          <input className="bg-[#101113]/90 p-2 rounded-md" type="text" placeholder="password" />
-          <button className="bg-[#101113]/90 p-2 rounded-md cursor-pointer">log in</button>
+          <input 
+            className="bg-[#101113]/90 p-2 rounded-md" 
+            type="text" 
+            placeholder="email"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          />
+          <input 
+            className="bg-[#101113]/90 p-2 rounded-md" 
+            type="password" 
+            placeholder="password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="bg-[#101113]/90 p-2 rounded-md cursor-pointer">log in</button>
         </div>
-        ): 
-        null
-        }
-
       </form>
-      {/* <h1>admin</h1>
-      <div className="flex flex-col z-50">
-      <input
-        type="text"
-        placeholder="post.."
-      />
-      <button className="cursor-pointer" onClick={() => {console.log(`press`)}}>submit</button>
-      </div> */}
+    ): 
+    null
+    }
+
+    {isLoggedIn ? (
+      <div className="">
+        <h1>ur logged in vro</h1>
+      </div>
+    ):
+    null
+    }
+
     </div>
   );
 }
