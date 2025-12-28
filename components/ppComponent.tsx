3a2934f346874b913b3edc19df7be1c411669d4c
mierbear <175657293+mierbear@users.noreply.github.com>
2025-12-28 01:04:20 -0400

    set up buttons functionality in pp page

--- components/ppComponent.tsx
+++ components/ppComponent.tsx
@@ -16,6 +16,8 @@ const font2 = Questrial({
 export default function Home() {
   const marqueeRef = useRef<HTMLParagraphElement | null>(null);
 
+  const currentYear = new Date().getFullYear();
+
   useEffect(() => {
     if (!marqueeRef.current) return;
 
@@ -28,7 +30,9 @@ export default function Home() {
   const [ready, setReady] = useState(false);
   useEffect(() => setReady(true), []);
 
-  const news = [`--- the secret santa event is over, thank you to those who participated <3 check out the secret santa gallery to see the results! --- have a merry christmas and a happy new year! ---`];
+  const news = [`— the secret santa event is over, thank you to those who participated <3 check out the secret santa gallery to see the results! — have a merry christmas and a happy new year! —`];
+
+  const [currentTab, setCurrentTab] = useState<"about" | "lore" | "characters">("about")
   
   return (
     // <main className="min-w-screen min-h-[300vh] align-center items-center flex flex-col z-50 bg-linear-to-b from-[rgb(8,173,223)] via-[rgb(20,86,140)] to-[rgb(33,50,67)]">
@@ -37,7 +41,7 @@ export default function Home() {
       </div>
 
        
-      <div className="flex flex-col z-100 min-w-screen lg:min-w-[80vw]">
+      <div className="flex flex-col z-100 min-w-screen lg:min-w-[60vw]">
         <div className="flex flex-col justify-end pt-[10vh]">
           <h1 className={`${font.className} nonsel text-[#1a1d20] text-shadow-md text-6xl z-50 translate-y-1`}>
             PACIFIC <span className="text-red-600">🞨</span> PURGATORY
@@ -56,8 +60,57 @@ export default function Home() {
             </p>
           </div>
           
-          <div className="">
+          {/* CAROUSEL */}
+          <div className="min-h-[40vh] max-h-[40vh] bg-black/50">
 
+          </div>
+
+          <div className="grid grid-cols-[1fr_4fr]">
+            <div className="grid grid-rows-1 pt-4">
+              
+              <button onClick={() => {setCurrentTab("about")}} className={`${font2.className} bg-white cursor-pointer rounded p-4 text-center m-4 mt-0`}>
+                About
+              </button>
+              
+              <button onClick={() => {setCurrentTab("characters")}} className={`${font2.className} bg-white cursor-pointer rounded p-4 text-center m-4 mt-0`}>
+                Characters
+              </button>
+
+              <button onClick={() => {setCurrentTab("lore")}} className={`${font2.className} bg-white cursor-pointer rounded p-4 text-center m-4 mt-0`}>
+                "Lore"
+              </button>
+              
+              <button className={`${font2.className} bg-white cursor-pointer rounded p-4 text-center m-4 mt-0`}>
+                Collabs
+              </button>
+              
+              <button className={`${font2.className} bg-white cursor-pointer rounded p-4 text-center m-4 mt-0`}>
+                News
+              </button>
+              
+              <button className={`${font2.className} bg-white cursor-pointer rounded p-4 text-center m-4 mt-0`}>
+                Submit Story
+              </button>
+
+            </div>
+
+            <div className="bg-white/20 flex flex-col items-center">
+              {currentTab === "about" && (
+                <div className="flex items-center text-center flex-col">
+                  <h1 className={`${font2.className} text-3xl pt-4`}>About</h1>
+                  <p className={`${font2.className}`}>Pacific Purgatory is a small online community of lovely artists that I <i>somehow</i> managed to cultivate. </p>
+
+                  <h1 className={`${font2.className} text-3xl pt-4`}>How to Join?</h1>
+                  
+                </div>
+              )}
+              {currentTab === "lore" && (
+                <p>lore</p>
+              )}
+              {currentTab === "characters" && (
+                <p>characters</p>
+              )}
+            </div>
           </div>
 
         </div>
@@ -66,6 +119,8 @@ export default function Home() {
 
       <div className="invisible md:flex">
       </div>
+
+      
 
       <video autoPlay muted loop className="object-cover fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-[120vw] min-h-[120vh] blur-[10px]">
         <source src="videos/pp.mp4" type="video/mp4" />
