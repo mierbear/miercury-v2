12946c0e9ff1d54fe490288b725a23e30be083e5
mierbear <175657293+mierbear@users.noreply.github.com>
2026-01-04 05:12:11 -0400

    implement blog pagination and render

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,8 +1,65 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 import BlogComponent from "@/components/blogComponent";
+import supabase from "@/lib/supabaseClient";
+import { useParams } from "next/navigation";
+import PostType from "@/types/postType";
 
-export default function Home() {
+const POSTS_PER_PAGE = 3;
+
+export default function Home({
+  params,
+}: {
+  params: { page: string };
+}) {
+  const parameters = useParams();
+  const page = Number(parameters.page) || 1;
+  const from = (page - 1) * POSTS_PER_PAGE;
+  const to = from + POSTS_PER_PAGE - 1;
+
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const [currentPosts, setCurrentPosts] = useState<PostType[]>([]);
+
+  const fetchPosts = async () => {
+    const { error, data } = await supabase
+      .from("posts")
+      .select("*")
+      .order("date_created", { ascending: false })
+
+    if (error) {
+      console.error("fetch failed: ", error.message);
+      return;
+    }
+    
+    setPosts(data);
+  }
+
+  const fetchCurrentPosts = async () => {
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*")
+      .order("date_created", { ascending: false })
+      .range(from, to);
+
+    if (error) {
+      console.error("fetch failed: ", error.message);
+      return;
+    }
+    
+    setCurrentPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    fetchCurrentPosts();
+  }, []);
+
   return (
-    <BlogComponent />
+    <BlogComponent
+      p={posts}
+      cp={currentPosts}
+      pg={page}
+      ppp={POSTS_PER_PAGE}
+    />
   );
 }
--- components/blogComponent.tsx
+++ components/blogComponent.tsx
@@ -1,65 +1,102 @@
 "use client";
 import gsap from "gsap";
 import Image from "next/image";
-import supabase from "@/lib/supabaseClient";
 import { useEffect, useRef, useState } from "react";
-import { useParams } from "next/navigation";
 import PostType from "@/types/postType";
 import NextLink from "next/link";
 
-export default function Blog() {
-  
-  const [posts, setPosts] = useState<PostType[]>([]);
-  
-  const fetchPosts = async () => {
-    const { error, data } = await supabase
-      .from("posts")
-      .select("*")
-      .order("date_created", { ascending: false })
+interface BlogComponentProps {
+  p: PostType[];
+  cp: PostType[];
+  pg: number;
+  ppp: number;
+}
 
-    if (error) {
-      console.error("fetch failed: ", error.message);
-      return;
-    }
-    
-    setPosts(data);
+export default function Blog({
+  p,
+  cp,
+  pg,
+  ppp
+  } : BlogComponentProps
+  ){
+
+  const [properDate, setProperDate] = useState(false);
+  
+  const clickDate = () => {
+    setProperDate(!properDate);
   }
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+  const posts = p;
+  const currentPosts = cp;
+  const currentPage = pg;
+  const PPP = ppp;
 
+  const totalPages = Math.ceil(posts.length / PPP);
 
   return (
     <main className="min-w-screen min-h-screen align-center items-center flex flex-col z-50 text-white">
       <div className="min-h-80 w-240 flex flex-col justify-center items-center z-50">
         <h1>blog</h1>
       </div>
+
       <div className="grid grid-cols-[1fr_2fr] w-240 min-h-screen z-50">
 
         <div className="bg-[#adb7be]/20 z-50 flex flex-col items-center">
           <h1 className="py-4 font-bold">archive</h1>
-          {posts.map((post) => {
-            return (
-              <div key={post.id} className="rounded-md max-w-[85ch] w-full flex flex-row items-center justify-between pl-4 pr-4 z-50">
 
-                <NextLink href={`/blog/post/${post.slug}`}>
-                  <p className="hover:underline blue">{post.title}</p>
-                </NextLink>
-                
-                <div className="text-xs text-gray-400 nonsel flex">
-                  <p className="">— {post.date}</p>
-                </div>
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              className="rounded-md max-w-[85ch] w-full flex flex-row items-center justify-between pl-4 pr-4 z-50"
+            >
+              <NextLink href={`/blog/post/${post.slug}`}>
+                <p className="hover:underline text-blue-600">{post.title}</p>
+              </NextLink>
 
+              <div className="text-xs text-gray-400 select-none flex">
+                <p>— {post.date}</p>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
+
 
         <div className="bg-[#c9d3d6]/20">
-          
+          {currentPosts === null ? null : currentPosts.map((post) => {
+            return (
+                <div key={post.id} className="post p-5 rounded-md mb-2 max-w-[85ch] w-full">
+                  <h1 className="font-bold text-2xl">{post.title}</h1>
+                  <div className="text-xs pt-0.5 text-gray-400 nonsel flex" onClick={clickDate}>
+                    <p className="underline">{properDate ? (post.spec_date) : post.date}</p>
+                    {post.updated_date === null ? null : (<p className="pl-5">last updated at:</p>)}
+                    {post.updated_spec_date && properDate ? (<p className="pl-2 underline">{post.updated_spec_date}</p>) : (<p className="pl-2 underline">{post.updated_date}</p>)}
+                  </div>
+                  <div
+                    className="prose prose-invert pt-5 max-w-none"
+                    dangerouslySetInnerHTML={{ __html: post.content }}
+                  />
+                  <hr className="my-6 border-neutral-500/40 max-w-[80ch] w-full translate-y-6.5" />
+              </div>
+            )
+          })}
         </div>
+          
+      </div>
 
+      <div>
+        <div className="flex justify-between pt-6">
+          {currentPage > 1 ? (
+            <NextLink href={`/blog/page/${currentPage - 1}`}>
+              ← Previous
+            </NextLink>
+          ) : <span />}
+
+          {currentPage < totalPages && (
+            <NextLink href={`/blog/page/${currentPage + 1}`}>
+              Next →
+            </NextLink>
+          )}
+        </div>
       </div>
     </main>
   )
