e940bdc436ac41e98e3957886d405608a9e5c3da
mierbear <175657293+mierbear@users.noreply.github.com>
2026-01-16 16:25:23 -0400

    implement logic for showing carousel

--- components/aboutComponent.tsx
+++ components/aboutComponent.tsx
@@ -11,37 +11,451 @@ export default function Home() {
     });
 
   const favAnime = [
-    {  comment: "its my absolute favorite. chokes me up when i think about it vro..", tag: "best", name: "Gurren Lagann", img: "gurrenlagann.jpg" },
-    {  comment: "", tag: "best", name: "Code Geass", img: "codegeass.jpg" },
-    {  comment: "crazy tearjerker", tag: "love", name: "Ranking of Kings", img: "rankingofkings.jpg" },
-    {  comment: "", tag: "love", name: "Spy x Family", img: "spyxfamily.jpg" },
-    {  comment: "", tag: "", name: "The Apothecary Diaries", img: "apothecarydiaries.jpg" },
-    {  comment: "", tag: "", name: "My Happy Marriage", img: "myhappymarriage.jpg" },
-    {  comment: "", tag: "", name: "From Bureaucrat to Villainess: Dad's Been Reincarnated!", img: "villainess.jpg" },
-    {  comment: "", tag: "", name: "Welcome to Demon School! Iruma-kun", img: "iruma.jpg" },
-    {  comment: "", tag: "", name: "Migi & Dali", img: "migianddali.jpg" },
-    {  comment: "", tag: "", name: "Assassination Classroom", img: "assassinationclassroom.jpg" },
-    {  comment: "", tag: "", name: "One Piece", img: "onepiece.jpg" },
-    {  comment: "", tag: "", name: "Odd Taxi", img: "oddtaxi.jpg" },
-    {  comment: "", tag: "", name: "Cowboy Bebop", img: "cowboybebop.jpg" },
-    {  comment: "", tag: "", name: "Paprika", img: "paprika.jpg" },
-    {  comment: "", tag: "", name: "Detective Conan", img: "detectiveconan.jpg" },
-    {  comment: "", tag: "", name: "Frieren: Beyond Journey's End", img: "frieren.jpg" },
-    {  comment: "", tag: "love", name: "To Your Eternity", img: "toyoureternity.jpg" },
-    {  comment: "", tag: "love", name: "Made in Abyss", img: "madeinabyss.jpg" },
-    {  comment: "", tag: "love", name: "Vinland Saga", img: "vinlandsaga.jpg" },
-    {  comment: "", tag: "best", name: "Monster", img: "monster.jpg" },
-    {  comment: "", tag: "best", name: "Gankutsuou: The Count of Monte Cristo", img: "gankutsuou.jpg" },
+    {  
+      name: "Gurren Lagann",
+      img: "gurrenlagann.jpg",
+      comment: "its my absolute favorite. chokes me up when i think about it vro..",
+      tag: "best",
+    },
+    {  
+      name: "Code Geass",
+      img: "codegeass.jpg",
+      comment: "",
+      tag: "best",
+    },
+    {  
+      name: "Ranking of Kings",
+      img: "rankingofkings.jpg",
+      comment: "crazy tearjerker",
+      tag: "love",
+    },
+    {  
+      name: "Spy x Family",
+      img: "spyxfamily.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {  
+      name: "The Apothecary Diaries",
+      img: "apothecarydiaries.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "My Happy Marriage",
+      img: "myhappymarriage.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "From Bureaucrat to Villainess: Dad's Been Reincarnated!",
+      img: "villainess.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Welcome to Demon School! Iruma-kun",
+      img: "iruma.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Migi & Dali",
+      img: "migianddali.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Assassination Classroom",
+      img: "assassinationclassroom.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "One Piece",
+      img: "onepiece.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Odd Taxi",
+      img: "oddtaxi.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Cowboy Bebop",
+      img: "cowboybebop.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Paprika",
+      img: "paprika.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Detective Conan",
+      img: "detectiveconan.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "Frieren: Beyond Journey's End",
+      img: "frieren.jpg",
+      comment: "",
+      tag: "",
+    },
+    {  
+      name: "To Your Eternity",
+      img: "toyoureternity.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {  
+      name: "Made in Abyss",
+      img: "madeinabyss.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {  
+      name: "Vinland Saga",
+      img: "vinlandsaga.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {  
+      name: "Monster",
+      img: "monster.jpg",
+      comment: "",
+      tag: "best",
+    },
+    {  
+      name: "Gankutsuou: The Count of Monte Cristo",
+      img: "gankutsuou.jpg",
+      comment: "",
+      tag: "best",
+    },
   ];
+
+  const favGames = [
+    {
+      name: "Rimworld",
+      img: "rimworld.jpg",
+      comment: "everytime i start the game i end up playing for more than 20 hours xd",
+      tag: "best",
+    },
+    {
+      name: "Risk of Rain 2",
+      img: "ror2.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "osu!",
+      img: "osu.jpg",
+      comment: "7k mania is my crack cocaine...",
+      tag: "best",
+    },
+    {
+      name: "Skyrim",
+      img: "skyrim.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Starbound",
+      img: "starbound.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {
+      name: "Terraria",
+      img: "terraria.jpg",
+      comment: "",
+      tag: "love",
+    },
+
+    {
+      name: "Undertale",
+      img: "undertale.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Omori",
+      img: "omori.jpg",
+      comment: "legit actually mid but still holds a really special place in my heart lol",
+      tag: "",
+    },
+
+    {
+      name: "Dead Cells",
+      img: "deadcells.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Escape from Duckov",
+      img: "duckov.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Echo Point Nova",
+      img: "echopointnova.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Elden Ring",
+      img: "eldenring.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Monster Hunter Rise",
+      img: "mhr.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Fear and Hunger",
+      img: "fah.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Balatro",
+      img: "balatro.jpg",
+      comment: "i love gambling",
+      tag: "",
+    },
+    {
+      name: "Slay The Spire",
+      img: "slaythespire.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Cry of Fear",
+      img: "cof.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Metaphor: ReFantazio",
+      img: "metaphor.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Minecraft",
+      img: "minecraft.jpg",
+      comment: "rlcraft is the only thing that makes me wanna play it ngl",
+      tag: "love",
+    },
+    {
+      name: "Left 4 Dead 2",
+      img: "l4d2.jpg",
+      comment: "",
+      tag: "",
+    },
+  ];
+
+  const favMusic = [
+    {
+      name: "TAK / DORIDORI",
+      img: "tak.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {
+      name: "Chikoi The Maid",
+      img: "chikoi.jpg",
+      comment: "",
+      tag: "love",
+    },
+
+    {
+      name: "Pink Guy",
+      img: "pinkguy.jpg",
+      comment: "",
+      tag: "best",
+    },
+    {
+      name: "Joji",
+      img: "joji.jpg",
+      comment: "",
+      tag: "best",
+    },
+
+    {
+      name: "Sasuke Haraguchi",
+      img: "sasukeharaguchi.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {
+      name: "Deco*27",
+      img: "deco27.jpg",
+      comment: "",
+      tag: "love",
+    },
+
+    {
+      name: "Uplift Spice",
+      img: "upliftspice.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Kikuo",
+      img: "kikuo.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Kinoue64",
+      img: "kinoue64.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Dusqk",
+      img: "dusqk.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Porter Robinson",
+      img: "porterrobinson.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {
+      name: "Kanye West",
+      img: "kanye.jpg",
+      comment: "",
+      tag: "love",
+    },
+
+    {
+      name: "Kinoko Teikoku",
+      img: "kinokoteikoku.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "My Dead Girlfriend",
+      img: "mydeadgirlfriend.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Pacific Purgatory",
+      img: "pacificpurgatory.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Steakfry",
+      img: "steakfry.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "Creepy Nuts",
+      img: "creepynuts.jpg",
+      comment: "",
+      tag: "love",
+    },
+    {
+      name: "Vaundy",
+      img: "vaundy.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "xi",
+      img: "xi.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "ginkiha",
+      img: "ginkiha.jpg",
+      comment: "",
+      tag: "",
+    },
+
+    {
+      name: "System of a Down",
+      img: "soad.jpg",
+      comment: "",
+      tag: "",
+    },
+    {
+      name: "Linkin Park",
+      img: "linkinpark.jpg",
+      comment: "",
+      tag: "love",
+    },
+  ];
+
+  const [activeList, setActiveList] = useState<"anime" | "music" | "games" | null>(null);
+
+  const openGames = () => {
+    if (activeList === "games") {
+      closeList();
+    } else {
+      setActiveList("games");
+    }
+  }
+
+  const openMusic = () => {
+    if (activeList === "music") {
+      closeList();
+    } else {
+      setActiveList("music");
+    }
+  }
+
+  const openAnime = () => {
+    if (activeList === "anime") {
+      closeList();
+    } else {
+      setActiveList("anime");
+    }
+  }
+
+  const closeList = () => {
+    setActiveList(null);
+  }
 
   return (
     <div className="min-w-screen min-h-screen max-h-screen flex flex-col items-center justify-center">
       
-    <div className="h-screen md:w-[60vw] grid grid-cols-[2fr_3fr]">
+    <div className="h-screen xl:w-[60vw] lg:w-[80vw] w-screen grid grid-cols-[2fr_3fr]">
       <div className="bg-white flex flex-col items-center justify-center">
       </div>
-      <div className="bg-black flex flex-col items-center justify-center">
-        <p className="md:text-8xl lg:text-9xl sm:text-7xl text-6xl text-white font-bold">TEXTX</p>
+      <div className="bg-black flex flex-col items-center ">
+        <p className="md:text-8xl lg:text-9xl sm:text-7xl text-6xl text-white font-bold cursor-pointer" onClick={() => {openAnime()}}>anime</p>
+        <p className="md:text-8xl lg:text-9xl sm:text-7xl text-6xl text-white font-bold cursor-pointer" onClick={() => {openMusic()}}>music</p>
+        <p className="md:text-8xl lg:text-9xl sm:text-7xl text-6xl text-white font-bold cursor-pointer" onClick={() => {openGames()}}>games</p>
       </div>
     </div>
 
@@ -52,10 +466,16 @@ export default function Home() {
       >
         <div className="overflow-hidden flex items-center justify-center h-full w-full" ref={emblaRef}>
           <div className="flex h-full">
-            {favAnime.map((anime, index) => (
+            {activeList === "anime" && favAnime.map((anime, index) => (
               <div
                 key={index}
-                className="flex-[0_0_32%] sm:flex-[0_0_26%] md:flex-[0_0_20%] lg:flex-[0_0_10%] px-1 h-full flex flex-col items-center nonsel relative"
+                className="
+                flex-[0_0_28%]
+                sm:flex-[0_0_24%]
+                md:flex-[0_0_18%]
+                lg:flex-[0_0_14%]
+                xl:flex-[0_0_9%]
+                px-1 h-full flex flex-col items-center nonsel relative"
                 draggable="false"
               >
                 <div className="relative w-auto h-full overflow-hidden rounded-xl">
@@ -71,87 +491,59 @@ export default function Home() {
                 </div>
               </div>
             ))}
-            {/* {activeList === "music" && favMusic.map((music, index) => (
+
+            {activeList === "music" && favMusic.map((music, index) => (
               <div
                 key={index}
-                className="flex-[0_0_12%] flex flex-col items-center py-2 nonsel"
+                className="
+                flex-[0_0_43%]
+                sm:flex-[0_0_36%]
+                md:flex-[0_0_27%]
+                lg:flex-[0_0_21%]
+                xl:flex-[0_0_14%]
+                px-1 h-full flex flex-col items-center nonsel relative"
                 draggable="false"
               >
-                <div className="relative w-48 h-48 sm:w-40 sm:h-40 xl:w-64 xl:h-64">
+                <div className="relative w-auto h-full overflow-hidden rounded-xl">
                   <img
                     src={`/images/about/music/${music.img}`}
                     alt={music.name}
-                    sizes="
-                    (max-width: 640px) 128px,
-                    (max-width: 1280px) 160px,
-                    192px
-                    "
-                    className="poster rounded-xl object-cover"
-                    onMouseEnter={() => {hoverHandle(music.comment)}}
-                    onMouseLeave={unhoverHandle}
+                    className="h-full w-auto object-cover"
                   />
-                </div>
-                <p className={`text-white mt-2 text-center ${music.tag}`}>{music.name}</p>
-                <br></br>
-                <div className="relative w-48 h-48 sm:w-40 sm:h-40 xl:w-64 xl:h-64">
-                  <img
-                    src={`/images/about/music/${music.img2}`}
-                    alt={music.name2}
-                    sizes="
-                    (max-width: 640px) 128px,
-                    (max-width: 1280px) 160px,
-                    192px
-                    "
-                    className="poster rounded-xl object-cover"
-                    onMouseEnter={() => {hoverHandle(music.comment2)}}
-                    onMouseLeave={unhoverHandle}
-                  />
-                </div>
-                <p className={`text-white mt-2 text-center ${music.tag2}`}>{music.name2}</p>
-              </div>
-            ))} */}
 
-            {/* {activeList === "games" && favGames.map((game, index) => (
+                  <p className={`text-center absolute bottom-2 left-2 right-2 text-xs text-white bg-black/80 p-1 border border-white font-bold rounded-md ${music.tag}`}>
+                    {music.name}
+                  </p>
+                </div>
+              </div>
+            ))}
+
+            {activeList === "games" && favGames.map((games, index) => (
               <div
                 key={index}
-                className="flex-[0_0_12%] flex flex-col items-center py-2 nonsel"
+                className="
+                flex-[0_0_28%]
+                sm:flex-[0_0_24%]
+                md:flex-[0_0_18%]
+                lg:flex-[0_0_14%]
+                xl:flex-[0_0_9%]
+                px-1 h-full flex flex-col items-center nonsel relative"
                 draggable="false"
               >
-                <div className="relative w-32 h-48 sm:w-40 sm:h-60 xl:w-48 xl:h-72">
+                <div className="relative w-auto h-full overflow-hidden rounded-xl">
                   <img
-                    src={`/images/about/games/${game.img}`}
-                    alt={game.name}
-                    sizes="
-                    (max-width: 640px) 128px,
-                    (max-width: 1280px) 160px,
-                    192px
-                    "
-                    className="poster rounded-xl object-cover"
-                    onMouseEnter={() => {hoverHandle(game.comment)}}
-                    onMouseLeave={unhoverHandle}
+                    src={`/images/about/games/${games.img}`}
+                    alt={games.name}
+                    className="h-full w-auto object-cover"
                   />
-                </div>
 
-                  <p className={`text-white mt-2 text-center ${game.tag}`}>{game.name}</p>
-                <br></br>
-                <div className="relative w-32 h-48 sm:w-40 sm:h-60 xl:w-48 xl:h-72">
-                  <img
-                    src={`/images/about/games/${game.img2}`}
-                    alt={game.name2}
-                    sizes="
-                    (max-width: 640px) 128px,
-                    (max-width: 1280px) 160px,
-                    192px
-                    "
-                    className="poster rounded-xl object-cover"
-                    onMouseEnter={() => {hoverHandle(game.comment2)}}
-                    onMouseLeave={unhoverHandle}
-                  />
+                  <p className={`text-center absolute bottom-2 left-2 right-2 text-xs text-white bg-black/80 p-1 border border-white font-bold rounded-md ${games.tag}`}>
+                    {games.name}
+                  </p>
                 </div>
-                <p className={`text-white mt-2 text-center ${game.tag2}`}>{game.name2}</p>
               </div>
-            ))} */}
-
+            ))}
+            
           </div>
         </div>
       </div>
