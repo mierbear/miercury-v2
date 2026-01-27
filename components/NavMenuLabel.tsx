76d36a5e3ee2e3e2985ed87237ac25d44ee71881
mierbear <175657293+mierbear@users.noreply.github.com>
2026-01-26 20:11:47 -0400

    fix navmenu text anim duration mismatch

--- components/NavMenuLabel.tsx
+++ components/NavMenuLabel.tsx
@@ -18,19 +18,21 @@ export default function Label ({activeLink, title, desc, link}: LabelProps) {
   return (
     <div className={`absolute bottom-0 h-18 xl:h-24 pb-1 w-full bg-[#17191a]/80 flex flex-col items-center justify-center transition-all duration-400 ${activeLink === link ? "text-yellow-200 opacity-100" : "text-white opacity-30"}`}>
 
-      <p className={`text-xl md:text-2xl lg:text-3xl transition-transform duration-500 ${activeLink === link ? "translate-y-0 scale-100" : "translate-y-2.5 scale-80"} flex items-center gap-4`}>
-        <span className={`flex items-center justify-center origin-center text-3xl transition-scale duration-300 ease-in-out ${activeLink === link ? "spin scale-100" : "scale-80"}`}>
+      <div className={`text-3xl transition-transform duration-400 ${activeLink === link ? "translate-y-0 scale-100" : "translate-y-2.5 scale-80"} flex items-center gap-4`}>
+        <span className={`flex items-center justify-center origin-center transition-scale duration-400 ease-in-out ${activeLink === link ? "spin scale-100" : "scale-80"}`}>
         {activeLink === link ? "★" : "✦"}
         </span>
 
+        <span className={`flex items-center justify-center origin-center transition-scale duration-400 ease-in-out ${activeLink === link ? "scale-100 font-bold" : "scale-80"}`}>
         {title}
+        </span>
 
-        <span className={`flex items-center justify-center origin-center text-3xl transition-scale duration-300 ease-in-out ${activeLink === link ? "spin scale-100" : "scale-80"}`}>
+        <span className={`flex items-center justify-center origin-center transition-scale duration-400 ease-in-out ${activeLink === link ? "spin scale-100" : "scale-80"}`}>
         {activeLink === link ? "★" : "✦"}
         </span>
-      </p>
+      </div>
 
-      <p className={`text-sm transition-opacity duration-300 ${activeLink === link ? "opacity-100" : "opacity-0"} text-nowrap`}>{desc}</p>
+      <p className={`text-sm transition-opacity duration-400 ${activeLink === link ? "opacity-100" : "opacity-0"} text-nowrap`}>{desc}</p>
 
     </div>
   )
