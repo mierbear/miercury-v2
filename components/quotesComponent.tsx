3cf90f0942aa7372718f882fd6880c545e2cc5cc
mierbear <175657293+mierbear@users.noreply.github.com>
2026-02-11 05:52:54 -0400

    show quotes in quotes page

--- components/quotesComponent.tsx
+++ components/quotesComponent.tsx
@@ -1,9 +1,22 @@
 "use client";
+import quotes from "@/components/quotes";
 
 export default function QuotesComponent() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="text-white w-screen h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">quotes...</h1>
+      <div className="h-[60vh] w-240 p-8 overflow-y-auto thin-scrollbar flex flex-col mt-8">
+
+        {quotes.map((quote, index) => (
+          <div key={index}>
+            <div className={`grid grid-cols-[16px_1fr] gap-2`}>
+              <p className="nonsel pointer-events-none flex items-center justify-center">✦</p>
+              <p className="">{quote}</p>
+            </div>
+          {index !== quotes.length - 1 && (<hr className=" border-gray-500/30 w-full my-2" />)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
