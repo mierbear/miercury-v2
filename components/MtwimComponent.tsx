4933f81a1a7f2a170eee6ed7f86310e6d927cd4e
mierbear <175657293+mierbear@users.noreply.github.com>
2026-04-14 15:45:48 -0300

    update tailwind / crashout / setup mtwim header

--- components/MtwimComponent.tsx
+++ components/MtwimComponent.tsx
@@ -16,7 +16,9 @@ export default function Mtwim() {
       <div className="flex flex-col justify-center items-center h-screen w-full">
         <div className="bg-[#9eadb9] h-[16%] w-full"></div>
         <div className="bg-[#90b5d3] h-[68%] w-full relative items-center justify-center flex">
-          <Marquee speed={30} >
+
+          {/* MARQUEE */}
+          <Marquee speed={30} className="absolute" >
             <div className="flex items-center nonsel pointer-events-none">
               <p className={`text-[80vh] ${rozha.className} opacity-5`}>MIER THE ICE MAGE</p>
               <div className="h-[60vh] w-[60vh] flex slow-backwards-spin items-center justify-center">
@@ -24,9 +26,20 @@ export default function Mtwim() {
               </div>
             </div>
           </Marquee>
+
+          {/* IMAGES */}
           <img src="/images/mtwim/frank.png" className="z-50 scale-70 origin-bottom-right nonsel pointer-events-none absolute  right-60 bottom-0 figure-breathe-slow" />
           <img src="/images/mtwim/mier.png"  className="z-50 scale-70 origin-bottom-right nonsel pointer-events-none absolute -right-20 bottom-0 figure-breathe-medium" />
+          
+          {/* CTA */}
+          <div className="absolute h-full w-160 left-20 bg-black/30 z-70 flex gap-6 items-center justify-center flex-col">
+            <p className="text-6xl font-bold text-center px-10  ">Mier: The Weakest Ice Mage</p>
+            <p className="">sdfdsfdsfsdf</p>
+            <p className="px-12 py-8 text-xl bg-white rounded-2xl cursor-pointer">READ PROLOGUE</p>
+          </div>
+          
         </div>
+
         <div className="bg-[#000000] h-[16%] w-full z-60"></div>
       </div>
 
