1957efc7d9b8c928e65dcb4ad393afb05d6e3482
mierbear <175657293+mierbear@users.noreply.github.com>
2026-04-14 12:39:15 -0300

    set up mtwim header images and add comic pages

--- components/MtwimComponent.tsx
+++ components/MtwimComponent.tsx
@@ -3,16 +3,21 @@
 export default function Mtwim() {
 
   return (
-    <div className="min-w-screen min-h-screen max-h-screen max-w-screen flex flex-col items-center">
+    <div className="min-w-screen min-h-screen max-w-screen flex flex-col items-center">
 
       {/* TOP */}
-      <div className="flex flex-col justify-center items-center h-160 w-full bg-white">
-        <p>HEADER</p>
+      <div className="flex flex-col justify-center items-center h-screen w-full">
+        <div className="bg-[#9eadb9] h-[16%] w-full"></div>
+        <div className="bg-[#90b5d3] h-[68%] w-full  relative">
+          <img src="/images/mtwim/frank.png" className="scale-70 origin-bottom-right nonsel pointer-events-none absolute  right-70 bottom-0" />
+          <img src="/images/mtwim/mier.png"  className="scale-70 origin-bottom-right nonsel pointer-events-none absolute -right-10 bottom-0" />
+        </div>
+        <div className="bg-[#e48519] h-[16%] w-full"></div>
       </div>
 
-      <p>
+      <div className="h-50 w-7xl bg-white">
         CONTENT
-      </p>
+      </div>
 
     </div>
   )
