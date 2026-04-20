79d713eae92d920d22f1198b086dea2ed5aec463
mierbear <175657293+mierbear@users.noreply.github.com>
2026-04-20 15:41:23 -0300

    implement miers grid hover effect logic

--- components/OcsComponent.tsx
+++ components/OcsComponent.tsx
@@ -2,21 +2,48 @@
 import { useEffect, useState, useRef } from "react";
 
 export default function Ocs() {
+
+  const [selectedMier, setSelectedMier] = useState("");
+
   return (
     <div className="w-screen max-w-screen justify-center align-center items-center flex flex-col">
 
       {/* MIERS */}
       <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#d1f8ff]">
         
-        <div className="grid grid-cols-3 w-full">
-          <div className="w-full h-full">
-            <img src="/images/ocs/mierintro-icemage.png" />
+        <div
+          className={`
+            grid w-full h-full transition-grid duration-500
+            ${selectedMier === "icemage" ? "grid-cols-[3fr_1fr_1fr]" :
+              selectedMier === "angel" ? "grid-cols-[1fr_3fr_1fr]" :
+              selectedMier === "tyrant" ? "grid-cols-[1fr_1fr_3fr]" :
+              "grid-cols-[1fr_1fr_1fr]"
+            }
+          `}
+        >
+          <div className="w-full h-full flex justify-center">
+            <img
+              className="object-cover"
+              src="/images/ocs/mierintro-icemage.png"
+              onMouseEnter={() => setSelectedMier("icemage")}
+              onMouseLeave={() => setSelectedMier("")}
+            />
           </div>
-          <div className="w-full h-full">
-            <img src="/images/ocs/mierintro-angel.png" />
+          <div className="w-full h-full flex justify-center">
+            <img
+              className="object-cover"
+              src="/images/ocs/mierintro-angel.png"
+              onMouseEnter={() => setSelectedMier("angel")}
+              onMouseLeave={() => setSelectedMier("")}
+            />
           </div>
-          <div className="w-full h-full">
-            <img src="/images/ocs/mierintro-tyrant.png" />
+          <div className="w-full h-full flex justify-center">
+            <img
+              className="object-cover"
+              src="/images/ocs/mierintro-tyrant.png"
+              onMouseEnter={() => setSelectedMier("tyrant")}
+              onMouseLeave={() => setSelectedMier("")}
+            />
           </div>
         </div>
 
