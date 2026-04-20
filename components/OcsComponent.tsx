2886a31da5fe3a05a8d1ad4b654220e0cb3e8a49
mierbear <175657293+mierbear@users.noreply.github.com>
2026-04-20 15:52:09 -0300

    implement proper overlap and z-indexing for miers

--- components/OcsComponent.tsx
+++ components/OcsComponent.tsx
@@ -13,7 +13,7 @@ export default function Ocs() {
         
         <div
           className={`
-            grid w-full h-full transition-grid duration-500
+            grid w-full h-full transition-grid duration-1000
             ${selectedMier === "icemage" ? "grid-cols-[3fr_1fr_1fr]" :
               selectedMier === "angel" ? "grid-cols-[1fr_3fr_1fr]" :
               selectedMier === "tyrant" ? "grid-cols-[1fr_1fr_3fr]" :
@@ -21,25 +21,25 @@ export default function Ocs() {
             }
           `}
         >
-          <div className="w-full h-full flex justify-center">
+          <div className="w-full h-full flex justify-center items-end">
             <img
-              className="object-cover"
+              className={`object-cover absolute z-22 ${selectedMier === "icemage" && "z-55"}`}
               src="/images/ocs/mierintro-icemage.png"
               onMouseEnter={() => setSelectedMier("icemage")}
               onMouseLeave={() => setSelectedMier("")}
             />
           </div>
-          <div className="w-full h-full flex justify-center">
+          <div className="w-full h-full flex justify-center items-end">
             <img
-              className="object-cover"
+              className={`object-cover absolute z-33`}
               src="/images/ocs/mierintro-angel.png"
               onMouseEnter={() => setSelectedMier("angel")}
               onMouseLeave={() => setSelectedMier("")}
             />
           </div>
-          <div className="w-full h-full flex justify-center">
+          <div className="w-full h-full flex justify-center items-end">
             <img
-              className="object-cover"
+              className={`object-cover absolute z-11 ${selectedMier === "tyrant" && "z-55"}`}
               src="/images/ocs/mierintro-tyrant.png"
               onMouseEnter={() => setSelectedMier("tyrant")}
               onMouseLeave={() => setSelectedMier("")}
