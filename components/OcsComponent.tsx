326903c9c187277e89cf1f8dd2ffe381f8b8ef72
mierbear <175657293+mierbear@users.noreply.github.com>
2026-04-20 20:08:28 -0300

    polish hover animations more

--- components/OcsComponent.tsx
+++ components/OcsComponent.tsx
@@ -3,67 +3,74 @@ import { useEffect, useState, useRef } from "react";
 
 export default function Ocs() {
 
-  const [selectedMier, setSelectedMier] = useState("");
+  const [hoveredMier, setHoveredMier] = useState("");
 
   return (
     <div className="w-screen max-w-screen justify-center align-center items-center flex flex-col">
 
       {/* MIERS */}
-      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#d1f8ff]">
+      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col relative bg-[#d1f8ff]">
         
         <div
           className={`
             grid w-full h-full transition-grid duration-1000
-            ${selectedMier === "icemage" ? "grid-cols-[3fr_1fr_1fr]" :
-              selectedMier === "angel" ? "grid-cols-[1fr_3fr_1fr]" :
-              selectedMier === "tyrant" ? "grid-cols-[1fr_1fr_3fr]" :
+            ${hoveredMier === "icemage" ? "grid-cols-[3fr_1fr_1fr]" :
+              hoveredMier === "angel" ? "grid-cols-[1fr_3fr_1fr]" :
+              hoveredMier === "tyrant" ? "grid-cols-[1fr_1fr_3fr]" :
               "grid-cols-[1fr_1fr_1fr]"
             }
           `}
         >
+
+          {/* ICE MAGE */}
           <div
-            className="w-full h-full flex justify-center items-end"
-            onMouseEnter={() => setSelectedMier("icemage")}
-            onMouseLeave={() => setSelectedMier("")}
+            className="w-full h-full flex justify-center items-end cursor-pointer"
+            onMouseEnter={() => setHoveredMier("icemage")}
+            onMouseLeave={() => setHoveredMier("")}
           >
             <img
               className={`
-                object-cover absolute z-22 transition-[translate] duration-1200
-                nonsel pointer-events-none
-                ${selectedMier === "icemage" ? "z-55" :
-                  selectedMier === "tyrant" && "translate-x-[-25%]"
+                object-cover absolute z-22 transition-transform duration-1200
+                nonsel pointer-events-none origin-top
+                ${hoveredMier === "icemage" ? "z-55 scale-110 translate-x-[-5%] duration-1400" :
+                  hoveredMier === "tyrant" && "translate-x-[-25%]"
                 }
               `}
               src="/images/ocs/mierintro-icemage.png"
             />
           </div>
+
+          {/* ANGEL */}
           <div
-            className="w-full h-full flex justify-center items-end"
-            onMouseEnter={() => setSelectedMier("angel")}
-            onMouseLeave={() => setSelectedMier("")}
+            className="w-full h-full flex justify-center items-end cursor-pointer"
+            onMouseEnter={() => setHoveredMier("angel")}
+            onMouseLeave={() => setHoveredMier("")}
           >
             <img
               className={`
-                object-cover absolute z-33 transition-[translate] duration-800
-                nonsel pointer-events-none
-                ${selectedMier === "icemage" ? "translate-x-[10%]" :
-                  selectedMier === "tyrant" && "translate-x-[-10%]"
+                object-cover absolute z-33 transition-transform duration-800
+                nonsel pointer-events-none origin-top
+                ${hoveredMier === "icemage" ? "translate-x-[10%]" :
+                  hoveredMier === "angel" ? "scale-110 duration-1400" :
+                  hoveredMier === "tyrant" && "translate-x-[-10%]"
                 }
               `}
               src="/images/ocs/mierintro-angel.png"
             />
           </div>
+
+          {/* TYRANT */}
           <div
-            className="w-full h-full flex justify-center items-end"
-            onMouseEnter={() => setSelectedMier("tyrant")}
-            onMouseLeave={() => setSelectedMier("")}
+            className="w-full h-full flex justify-center items-end cursor-pointer"
+            onMouseEnter={() => setHoveredMier("tyrant")}
+            onMouseLeave={() => setHoveredMier("")}
           >
             <img
               className={`
-                object-cover absolute z-11 transition-[translate] duration-1200
-                nonsel pointer-events-none
-                ${selectedMier === "tyrant" ? "z-55" :
-                  selectedMier === "icemage" && "translate-x-[25%]"
+                object-cover absolute z-11 transition-transform duration-1200
+                nonsel pointer-events-none origin-top
+                ${hoveredMier === "tyrant" ? "z-55 scale-110 translate-x-[5%] duration-1400" :
+                  hoveredMier === "icemage" && "translate-x-[25%]"
                 }
               `}
               src="/images/ocs/mierintro-tyrant.png"
@@ -71,31 +78,34 @@ export default function Ocs() {
           </div>
         </div>
 
+        {/* <div className="absolute">
+
+        </div> */}
 
       </div>
 
       {/* KANIN */}
-      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#e0f8b9]">
+      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col relative bg-[#e0f8b9] z-100">
         KANIN
       </div>
 
       {/* SKULLBOUND */}
-      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#acaaa9]">
+      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col relative bg-[#acaaa9] z-200">
         SKULLBOUND
       </div>
 
       {/* QUINCE */}
-      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#9e937a]">
+      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col relative bg-[#9e937a] z-300">
         QUINCE
       </div>
 
       {/* SIMEON */}
-      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#303a8d]">
+      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col relative bg-[#303a8d] z-400">
         SIMEON
       </div>
 
       {/* PIO */}
-      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col bg-[#413f3d]">
+      <div className="w-screen max-w-screen h-screen max-h-screen justify-center align-center items-center flex flex-col relative bg-[#413f3d] z-500">
         PIO
       </div>
 
